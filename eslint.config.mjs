2248e0e4dcf64ef77cc616817d689395f98d7c9c
Samuka Vei <marcos.samuel.developer@gmail.com>
2025-05-03 17:13:04 +0000

    feat: adicionado metadados

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: { globals: globals.browser },
+  },
+  tseslint.configs.recommended,
+
+  {
+    files: ["**/*.ts"],
+    rules: {
+      "@typescript-eslint/explicit-function-return-type": "error",
+      "@typescript-eslint/explicit-module-boundary-types": "error",
+    },
+  },
+]);
--- src/exercises/bdex2.ts
+++ src/exercises/bdex2.ts
@@ -1,5 +1,4 @@
 export default function main(input: string): string {
   const [a, b] = input.split("\n").map(Number);
 
-  return `X = ${a + b}`;
 }
